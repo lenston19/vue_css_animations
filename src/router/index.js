0031bca2d0dadb16575f67ede8ff99f63242eac9
Dmitriy <dimon.smolonogov@gmail.com>
2022-01-17 14:03:44 +0300

    Add Snow page

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Сonfetti from '@/views/Сonfetti.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Сonfetti from "@/views/Сonfetti.vue";
+import Snow from "@/views/Snow.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/confetti',
-    name: 'Сonfetti',
-    component: Сonfetti
-  }
-]
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/confetti",
+        name: "Сonfetti",
+        component: Сonfetti,
+    },
+    {
+        path: "/snow",
+        name: "Snow",
+        component: Snow,
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Snow.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="snow-wrapper">
+    <div class="snow"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
